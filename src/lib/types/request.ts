4a119fabb4def6953d4fe588bf46d15e239073ea
aandrx <andrewliucodes@gmail.com>
2026-01-15 13:52:54 -0500

    type definitions for requests

--- src/lib/types/request.ts
+++ src/lib/types/request.ts
@@ -4,3 +4,22 @@ export enum RequestStatus {
   COMPLETED = "completed",
   REJECTED = "rejected",
 }
+
+export interface ItemRequest {
+  _id?: string;
+  requestorName: string;
+  itemRequested: string;
+  requestCreatedDate: Date;
+  lastEditedDate: Date;
+  status: RequestStatus;
+}
+
+export interface CreateItemRequest {
+  requestorName: string;
+  itemRequested: string;
+}
+
+export interface EditStatusRequest {
+  id: string;
+  status: RequestStatus;
+}
